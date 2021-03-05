7ad82a981470e4f8dc128d42ccca317a90b4abda
Jesus Lara <jelara99@gmail.com>
2021-03-05 02:13:10 +0000

    avance

---
+++ src/js/component/carta.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function Carta() {
+	return (
+		<div className="card" style={{ width: "25%" }}>
+			<img
+				className="card-img-top"
+				src=".../100px180/"
+				alt="Card image cap"
+			/>
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">fjbsdkjhfgbkjsdbgkjgk</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+}
---
+++ src/js/component/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<div>
+			<nav className="navbar navbar-light bg-light">
+				<div className="container-fluid">
+					<span className="navbar-text">
+						Navbar text with an inline element
+					</span>
+				</div>
+			</nav>
+		</div>
+	);
+}
